4d9bec4b44ed3e7564bfaee1a5f98fcbf27e95bd
huda <hudachishtee@gmail.com>
2026-02-18 21:48:54 +0300

    Initial Commit

---
+++ Reading/ReadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingApp.swift
+//  Reading
+//
+//  Created by Huda Chishtee on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
